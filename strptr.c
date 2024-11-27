5296dd5ca35decede7450b3defd5a4a59d79d4e7
get-D <divyatwa5@gmail.com>
2024-11-27 19:56:11 +0530

    Update strptr.c
    added a function called details

--- strptr.c
+++ strptr.c
@@ -1,31 +1,29 @@
 #include <stdio.h>
 
-struct book{
-        char title[50];
-        char author[20];
-        double price;
-        int pages;
-    };
-    
-int main()
-{
-    
-    struct book book1 = {"learn C", "dennis ritchie",675.50,325};
-    struct book book2 = {"Structured Computer Organisation", "Andrew S. Tanebaum",810.50,874};
+struct book {
+    char title[50];
+    char author[20];
+    double price;
+    int pages;
+};
+
+void details(struct book *strptr) {
+    printf("Title: %s\n", strptr->title);
+    printf("Author: %s\n", strptr->author);
+    printf("Price: %.2lf\n", strptr->price);
+    printf("Pages: %d\n", strptr->pages);
+}
+
+int main() {
+    struct book book1 = {"learn C", "dennis ritchie", 675.50, 325};
+    struct book book2 = {"Structured Computer Organisation", "Andrew S. Tanenbaum", 810.50, 874};
     struct book *strptr;
+
     strptr = &book1;
+    details(strptr);
     
-    printf("Title: %s\n", strptr-> title);
-    printf("author: %s\n", strptr-> author);
-    printf("price: %lf\n", strptr-> price);
-    printf("pages: %d\n", strptr-> pages);
-    
-    strptr= &book2;
-    
-    printf("Title: %s\n", strptr-> title);
-    printf("author: %s\n", strptr-> author);
-    printf("price: %lf\n", strptr-> price);
-    printf("pages: %d\n", strptr-> pages);
+    strptr = &book2;
+    details(strptr);
 
     return 0;
 }
